3eee05ee963c8bb13403ee24ce9c34e3c39cf642
Greg Rickaby <gregrickaby@gmail.com>
2016-04-15 10:34:21 -0500

    remove unused files

--- searchform.php
+++
@@ -1,5 +0,0 @@
-<form method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label for="search-field"><span class="screen-reader-text"><?php esc_html_e( 'To search this site, enter a search term', 'wdunderscores' ) ?></span></label>
-	<input class="search-field" id="search-field" type="text" name="s" value="<?php echo get_search_query() ?>" aria-required="false" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search', 'wdunderscores' ) ?>" />
-	<button><?php esc_html_e( 'Submit', 'wdunderscores' ); ?></button>
-</form>
