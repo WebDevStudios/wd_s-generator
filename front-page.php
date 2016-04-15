de8b8c30d75cbaddb3b92fac4a82e025d99b247a
Greg Rickaby <gregrickaby@gmail.com>
2016-04-15 10:24:07 -0500

    update the words

--- front-page.php
+++ front-page.php
@@ -14,7 +14,7 @@ get_header(); ?>
 				<div class="wrap">
 
 					<section class="content-section generator" role="main">
-						<h2>Create a wd_s based theme</h2>
+						<h2>Create a WordPress theme based on wd_s</h2>
 						<?php do_action( 'wds_wdunderscores_print_form' ); ?>
 					</section><!-- .generator -->
 
@@ -24,14 +24,14 @@ get_header(); ?>
 							<li>Answer the questions above</li>
 							<li>Click "Generate"</li>
 							<li>The script will do a find-replace and give you a .zip file</li>
-							<li>Download, extract, and place your new theme files into wp-content/themes</li>
-							<li>For more information on using wd_s, see the <a title="wd_s readme" href="https://github.com/WebDevStudios/wd_s/blob/master/README.md">wd_s readme</a>.</li>
+							<li>Download the zip file, extract, and place your new theme files into wp-content/themes</li>
+							<li>For more information on using wd_s, see the <a title="wd_s readme" href="https://github.com/WebDevStudios/wd_s/blob/master/README.md">wd_s readme</a></li>
 						</ol>
 					</section><!-- .install -->
 
 					<section class="content-section about">
 						<h2>About</h2>
-						<a href="https://github.com/WebDevStudios/wd_s">wd_s</a> is a fork of <a href="https://github.com/Automattic/_s">Automattic's _s</a>. It's used as the starter theme at <a href="https://webdevstudios.com">WebDevStudios</a>, a leading WordPress development agency.
+						<a href="https://github.com/WebDevStudios/wd_s">wd_s</a> is a fork of Automattic's <a href="https://github.com/Automattic/_s">_s</a>. It's used as the starter theme at <a href="https://webdevstudios.com">WebDevStudios</a>, a leading WordPress development agency.
 					</section><!-- .contribute -->
 
 					<section class="content-section contribute">
