e8c4aa34a035cec521aa6034d671d90c9b774880
Greg Rickaby <gregrickaby@gmail.com>
2016-05-11 09:59:03 -0500

    keep the tone to 3rd person

--- front-page.php
+++ front-page.php
@@ -33,7 +33,7 @@ get_header(); ?>
 					<h2>About</h2>
 					<p>Hi. I'm a starter theme called wd_s, or wdunderscores. I'm a theme meant for hacking so don't use me as a Parent Theme. Instead, try turning me into the next, most awesome, WordPress theme out there. That's what I'm here for!</p>
 					<p>I feature some of the web's most exciting technologies like: Gulp, LibSass, PostCSS, Bourbon, Neat, and BrowserSync to help make your development process fast and efficient. I'm also accessible, passing both WCAG 2.0AA and Section 508 standards out of the box.</p>
-					<p>Find out more about wd_s, <a href="https://webdevstudios.com/tags/wd_s/">on our blog</a> or check us out at <a href="https://github.com/webdevstudios/wd_s">Github</a>.
+					<p>Find out more about me <a href="https://webdevstudios.com/tags/wd_s/">on my blog</a> or check me out at <a href="https://github.com/webdevstudios/wd_s">Github</a>.
 				</section><!-- .contribute -->
 
 				<section class="content-section contribute">
