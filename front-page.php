978d643e1794d7bc561449866ccd1c73ef78d0db
Cameron <cdcampbell26@gmail.com>
2019-11-01 10:00:31 -0400

    Escape and internationalize all the homepage text.

--- front-page.php
+++ front-page.php
@@ -13,31 +13,32 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<div class="wrap">
 
+			<h2 class="title"><?php echo esc_html( 'Create a WordPress theme based on wd_s' ); ?></h2>
+
 				<section class="content-section generator" role="main">
-					<h2>Create a WordPress theme based on wd_s</h2>
 					<?php do_action( 'wds_wdunderscores_print_form' ); ?>
 				</section><!-- .generator -->
 
 				<section class="content-section install">
-					<h2>Generator instructions</h2>
+					<h2><?php echo esc_html( 'Generator instructions' ); ?></h2>
 					<ol>
-						<li>Answer the questions above</li>
-						<li>Click "Generate"</li>
-						<li>The script will do a find-replace and give you a .zip file</li>
-						<li>Download the zip file, extract, and place your new theme files into wp-content/themes</li>
-						<li>For more information on using wd_s, see the <a title="wd_s readme" href="https://github.com/WebDevStudios/wd_s/blob/master/README.md">wd_s readme</a></li>
+						<li><?php echo esc_html( 'Answer the questions above' ); ?></li>
+						<li><?php echo esc_html( 'Click "Generate"' ); ?></li>
+						<li><?php echo esc_html( 'The script will do a find-replace and give you a .zip file' ); ?></li>
+						<li><?php echo esc_html( 'Download the zip file, extract, and place your new theme files into wp-content/themes' ); ?></li>
+						<li><?php echo esc_html( 'For more information on using wd_s, see the ' ); ?><a title="wd_s readme" href="<?php echo esc_url( 'https://github.com/WebDevStudios/wd_s/blob/master/README.md' ); ?>"><?php echo esc_html( 'wd_s readme' ); ?></a></li>
 					</ol>
 				</section><!-- .install -->
 
 				<section class="content-section about">
-					<h2>About</h2>
-					<p>Hi. I'm a starter theme called wd_s, or wdunderscores. I'm a theme meant for hacking so don't use me as a Parent Theme. Instead, try turning me into the next, most awesome, WordPress theme out there. That's what I'm here for!</p>
-					<p>I feature some of the web's most exciting technologies like: Gulp, LibSass, PostCSS, Bourbon, Neat, and BrowserSync to help make your development process fast and efficient. I'm also accessible, passing both WCAG 2.0AA and Section 508 standards out of the box.</p>
-					<p>Find out more about me <a href="https://webdevstudios.com/tags/wd_s/">on my blog</a> or check me out at <a href="https://github.com/webdevstudios/wd_s">Github</a>.
+					<h2><?php echo esc_html( 'About' ); ?></h2>
+					<p><?php echo esc_html( 'Hi. I\'m a starter theme called wd_s, or wdunderscores. I\'m a theme meant for hacking so don\'t use me as a Parent Theme. Instead, try turning me into the next, most awesome, WordPress theme out there. That\'s what I\'m here for!' ); ?></p>
+					<p><?php echo esc_html( 'I feature some of the web\'s most exciting technologies like: Gulp, LibSass, PostCSS, Bourbon, Neat, and BrowserSync to help make your development process fast and efficient. I\'m also accessible, passing both WCAG 2.0AA and Section 508 standards out of the box.' ); ?></p>
+					<p><?php echo esc_html( 'Find out more about me' ); ?> <a href="<?php echo esc_html( 'https://webdevstudios.com/tags/wd_s/' ); ?>"><?php echo esc_html( 'on my blog' ); ?></a> <?php echo esc_html( 'or check me out at' ); ?> <a href="<?php echo esc_html( 'https://github.com/webdevstudios/wd_s' ); ?>"><?php echo esc_html( 'Github' ); ?></a>.
 				</section><!-- .contribute -->
 
 				<section class="content-section contribute">
-					<h2>Brought to you by these fine folks</h2>
+					<h2><?php echo esc_html( 'Brought to you by these fine folks' ); ?></h2>
 					<?php echo wds_wdunderscores_do_contributors(); ?>
 				</section><!-- .contribute -->
 
